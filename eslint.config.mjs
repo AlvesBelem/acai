ba0b720e230805ee23badec982af9f066026adcd
AlvesBelem <marcelocartoes.pa@gmail.com>
2025-10-30 19:25:36 -0300

    chore: update dependencies and devDependencies in package.json; modify TypeScript configuration in tsconfig.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,54 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from '@eslint/js'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-export default eslintConfig;
+// Correção: importar os plugins corretamente com `.default`
+const typescript = (await import('@typescript-eslint/eslint-plugin')).default
+const tsParser = (await import('@typescript-eslint/parser')).default
+const react = (await import('eslint-plugin-react')).default
+const reactHooks = (await import('eslint-plugin-react-hooks')).default
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+    },
+  },
+
+  {
+    files: ['**/*.jsx', '**/*.tsx'],
+    languageOptions: {
+      globals: {
+        React: 'readonly',
+      },
+    },
+    plugins: {
+      react: react,
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+
+  {
+    ignores: ['.next/**', 'build/**', 'out/**', 'next-env.d.ts'],
+  },
+]
